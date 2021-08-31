725f41ecfe9053e17106c49ad9ba5eee3213350e
klmnrth <89395929+klmnrth@users.noreply.github.com>
2021-08-31 21:52:32 +0200

    update

--- data/process_data.py
+++ data/process_data.py
@@ -78,7 +78,7 @@ def save_data(df, database_filename):
             Destination path for saving the database (.db)
     """
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('df_table', engine, index=False)
+    df.to_sql('df_table', engine, index=False, if_exists='replace')
 
 
 def main():
